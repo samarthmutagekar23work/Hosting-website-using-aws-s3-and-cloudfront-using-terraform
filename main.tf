60c0be64c9c4c5d262eb4429f2b47b39af7df5e5
samarthmutagekar23work <smarthmutagekar23work@gmail.com>
2026-07-26 19:33:40 +0530

    Add Terraform project files

---
+++ main.tf
@@ -0,0 +1,149 @@
+# -----------------------------
+# S3 Bucket
+# -----------------------------
+resource "aws_s3_bucket" "website" {
+  bucket_prefix = var.bucket_prefix
+}
+
+# -----------------------------
+# Block Public Access
+# -----------------------------
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# -----------------------------
+# Origin Access Control (OAC)
+# -----------------------------
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "oac-${var.bucket_prefix}"
+  description                       = "Origin Access Control for S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# -----------------------------
+# Upload Website Files
+# -----------------------------
+resource "aws_s3_object" "website_files" {
+  for_each = fileset("${path.module}/www", "**/*")
+
+  bucket = aws_s3_bucket.website.id
+  key    = each.value
+  source = "${path.module}/www/${each.value}"
+  etag   = filemd5("${path.module}/www/${each.value}")
+
+  content_type = lookup({
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    json = "application/json"
+    png  = "image/png"
+    jpg  = "image/jpeg"
+    jpeg = "image/jpeg"
+    gif  = "image/gif"
+    svg  = "image/svg+xml"
+    ico  = "image/x-icon"
+    txt  = "text/plain"
+  }, element(reverse(split(".", each.value)), 0), "application/octet-stream")
+}
+
+# -----------------------------
+# CloudFront Distribution
+# -----------------------------
+resource "aws_cloudfront_distribution" "s3_distribution" {
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "S3Origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+
+    target_origin_id = "S3Origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  price_class = "PriceClass_100"
+}
+
+# -----------------------------
+# Bucket Policy
+# -----------------------------
+resource "aws_s3_bucket_policy" "website" {
+
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+
+        Action = "s3:GetObject"
+
+        Resource = "${aws_s3_bucket.website.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+
+  depends_on = [
+    aws_cloudfront_distribution.s3_distribution
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_name" {
+  default = "techwithsamarth-101"
+}
+variable "bucket_prefix" {
+  description = "Prefix for the S3 bucket"
+  type        = string
+}
